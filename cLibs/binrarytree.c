2f7a54333bc3b1c10e16611d59ac00dd4f0e1aae
Eddie <42759839+nuEGil@users.noreply.github.com>
2025-11-25 13:52:34 -0600

    Update binrarytree.c

--- cLibs/binrarytree.c
+++ cLibs/binrarytree.c
@@ -3,7 +3,6 @@
 #include<string.h>
 #include<ctype.h>
 /* Following: https://en.wikipedia.org/wiki/Binary_tree
-
 I want to do a succinct encoding verion of this. information theoretical lower bounds 
 number of different binary trees on n nodes is C_n -- nth Catalan number (trees w identical 
 structure are identical) for large n, this is 4^n -- need at least about log2 4^n = 2n bits to 
@@ -15,11 +14,50 @@ hmm. long long is 64 bits.... so I can use 1 long long to represent a tree of de
 8 bytes... vs if I did an array of ints for this I'd have 64*4 = 256 bytes.  
 
 C doesnt have a hash table.... betrayed
-
 */
 
+typedef struct Node {
+	char letter;
+	unsigned int count;
+	struct Node* left;
+	struct Node* right;
+} Node;
 
+Node* make_node(char letter, int count) {
+	Node* n = malloc(sizeof(Node));
+	n->letter = letter;
+	n->count = count;
+	n->left = NULL;
+	n->right = NULL;
+	return n;
+}
 
+void insert(Node** root, char letter, int count) {
+	// empty tree case
+	if (*root==NULL) {
+		*root = make_node(letter, count);
+		return;
+	}
+
+	Node *curr = *root;
+
+	while (1) {
+		if (count < curr->count) {
+			if (curr->left == NULL) {
+				curr->left = make_node(letter, count);
+				return;
+			}
+			curr = curr->left;
+		}
+		else {
+			if (curr->right == NULL) {
+				curr->right = make_node(letter, count);
+				return;
+			}
+			curr = curr->right;
+		}
+	}
+}
 
 int main() {
 	char text[] = "In computer science, a binary tree is a tree data structure in which each node has at most two children, \
@@ -32,22 +70,28 @@ rather than directed graph, in which case a binary tree is an ordered, rooted tr
 Some authors use rooted binary tree instead of binary tree to emphasize the fact that the tree is rooted, but as defined above, \
 a binary tree is always rooted.[6]\0";
 
-	// there are like 1000 characters here.... wrong type on character array
-	long long int countArray[26] = { 0 }; // if no hash map then I can just use the array here. 
+	// there are like 1000 characters here.... ok if all characters are equal prob- 1/26 * 1000 -> E=38 for each char... 
+	// we can get away with unsinged int here, it's a count
+	unsigned int countArray[26] = { 0 }; // if no hash map then I can just use the array here. 
 	
 	for (int i = 0; text[i]; ++i) { // keeps running till you hit \0
-		int id = (int)tolower(text[i]) - 97; // use 'a' as 0
-		//printf("char: %c id: %d i: %d\n", tolower(text[i]), id, i); // printf instead of debugger for now
-		if (id < 26) { // int has wrap around of negative numbers  -- so you just have to check if it's less than 26 to work 
+		int id = (int)tolower(text[i]) - 97;
+		if (id < 26 && id >= 0) { // logical and
 			countArray[id] += 1;
+			//printf("char: %c id: %d i: %d count: %d\n", tolower(text[i]), id, i, countArray[id]); // printf instead of debugger for now
 		}
 	}
-	 	
+	
+	Node *root = NULL;
+	// printing the array and build the tree
 	for (int i = 0; i < 26; ++i) {
-		printf("%c:[%lld]",'a'+i, countArray[i]);
+		printf("%c:[%d]",'a'+i, countArray[i]);
+		insert(&root, 'a' + i, countArray[i]);
+
 	}
 	printf("\n");
-	
+
+		
 	return EXIT_SUCCESS;
 
 }
